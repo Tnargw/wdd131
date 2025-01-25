1a0b62af774611911040f50215a494ee21067299
Grant Watson <94023954+Tnargw@users.noreply.github.com>
2025-01-24 18:36:22 -0700

    final

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -2,6 +2,7 @@
 const menuButton = document.querySelector(".menu-button");
 const menuItems = document.querySelector(".menu-options");
 
+/* EventListener for button click */
 menuButton.addEventListener("click", () => {
     menuItems.classList.toggle("show");
 });
