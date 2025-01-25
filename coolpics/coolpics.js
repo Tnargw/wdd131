42ca7a61652fc3cd763f3b0e599a6ac75661b164
Grant Watson <94023954+Tnargw@users.noreply.github.com>
2025-01-24 18:02:49 -0700

    Deleted file

--- coolpics/coolpics.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.querySelector(".menu-button");
-const menuItems = document.querySelector(".menu-options");
-
-menuButton.addEventListener("click", () => {
-    console.log("Menu button clicked");
-    menuItems.classList.toggle("show");
-});
